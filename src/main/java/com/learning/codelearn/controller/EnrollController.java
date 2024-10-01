d460081b710490ba641e364cca2a3e130d026d5b
Nhun231 <151536592+Nhun231@users.noreply.github.com>
2024-10-01 15:07:52 +0700

    Update EnrollController.java

--- src/main/java/com/learning/codelearn/controller/EnrollController.java
+++ src/main/java/com/learning/codelearn/controller/EnrollController.java
@@ -1,19 +1,12 @@
 package com.learning.codelearn.controller;
 
 import com.learning.codelearn.models.User;
-import com.learning.codelearn.repository.CourseRepository;
-import com.learning.codelearn.repository.LessonRepository;
-import com.learning.codelearn.repository.UserRepository;
-import com.learning.codelearn.service.CourseService;
 import com.learning.codelearn.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Controller
 public class EnrollController {
