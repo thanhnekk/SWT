789c4017019170bb9eeadb0d291bb9d7372768a6
Lin Linh <86091845+linlinh75@users.noreply.github.com>
2024-10-01 15:35:43 +0700

    Update FileUploadController.java

--- src/main/java/com/learning/codelearn/uploadingfiles/FileUploadController.java
+++ src/main/java/com/learning/codelearn/uploadingfiles/FileUploadController.java
@@ -13,46 +13,46 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
 import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
     private final StorageService storageService;
+
     @Autowired
     public FileUploadController(StorageService storageService) {
         this.storageService = storageService;
     }
+
     @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
-
+    public String listUploadedFiles(Model model) {
         model.addAttribute("files", storageService.loadAll().map(
-                        path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                                "serveFile", path.getFileName().toString()).build().toUri().toString())
-             (Stream.toList()));
-
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));  // Corrected the syntax for Collectors.toList()
         return "file";
     }
+
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
         Resource file = storageService.loadAsResource(filename);
-        if (file == null)
+        if (file == null) {
             return ResponseEntity.notFound().build();
-
+        }
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
+
     @PostMapping("/")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
-
         storageService.store(file);
         redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
-
         return "redirect:/";
     }
+
     @ExceptionHandler(StorageFileNotFoundException.class)
     public ResponseEntity<Void> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
