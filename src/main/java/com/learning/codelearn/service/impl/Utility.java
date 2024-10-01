59853c67a0796d49ee89be3c20afa66568610973
thanhnekk <thanhthanh16102004@gmail.com>
2024-10-01 15:05:53 +0700

    Update Utility.java

--- src/main/java/com/learning/codelearn/service/impl/Utility.java
+++ src/main/java/com/learning/codelearn/service/impl/Utility.java
@@ -3,6 +3,10 @@ package com.learning.codelearn.service.impl;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class Utility {
+
+    private Utility() {
+    }
+
     public static String getSiteURL(HttpServletRequest request) {
         String siteURL = request.getRequestURL().toString();
         return siteURL.replace(request.getServletPath(), "");
